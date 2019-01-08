e590d5e9a0a9d84b43632fe4178f66b6bd5001cb
LuWei <luwei@ykecf.cn>
2019-01-08 16:27:16 +0800

    gestrue change vol

--- app/src/main/java/com/loosu/soplayer/widget/videoview/SoVideoView.java
+++ app/src/main/java/com/loosu/soplayer/widget/videoview/SoVideoView.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.View;
 
@@ -26,6 +27,9 @@ public class SoVideoView extends AbsSoVideoView implements View.OnClickListener 
     private AutoFixSurfaceView mSurfaceView;
 
     private IController mController;
+
+    private float mDownX;
+    private float mDownY;
 
     public SoVideoView(@NonNull Context context) {
         this(context, null);
@@ -118,5 +122,35 @@ public class SoVideoView extends AbsSoVideoView implements View.OnClickListener 
         } else {
             mController.show();
         }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mDownX = ev.getX();
+                mDownY = ev.getY();
+                getParent().requestDisallowInterceptTouchEvent(true);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float dx = ev.getX() - mDownX;
+                float dy = ev.getY() - mDownY;
+                if (Math.abs(dx) < Math.abs(dy) && Math.abs(dy) > 50) {
+                    // 垂直滑动
+                    getParent().requestDisallowInterceptTouchEvent(false);
+
+                } else {
+                    // 水平滑动
+                    getParent().requestDisallowInterceptTouchEvent(true);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                if (getParent() != null) {
+                    getParent().requestDisallowInterceptTouchEvent(false);
+                }
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
--- app/src/main/java/com/loosu/soplayer/widget/videoview/controller/GestureController.java
+++ app/src/main/java/com/loosu/soplayer/widget/videoview/controller/GestureController.java
@@ -1,30 +1,30 @@
 package com.loosu.soplayer.widget.videoview.controller;
 
 import android.content.Context;
+import android.media.AudioManager;
 import android.support.annotation.NonNull;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
-import android.view.View;
 import android.view.ViewConfiguration;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import com.loosu.soplayer.R;
 import com.loosu.soplayer.utils.KLog;
+import com.loosu.soplayer.widget.SoProgressBar;
 
 public class GestureController extends Controller {
     private static final String TAG = "GestureController";
 
-    private final int mTouchSlop;
+    private boolean mVolChanging = false;
+    private int mVolume;
 
-    private float mLastMotionX;
-    private float mLastMotionY;
+    private SoProgressBar mProgressVolume;
 
-    private View mLayoutVolumeChange;
+    private final GestureDetector mGestureDetector;
 
     public GestureController(@NonNull Context context) {
         super(context);
-        ViewConfiguration configuration = ViewConfiguration.get(context);
-        mTouchSlop = configuration.getScaledPagingTouchSlop();
+        mGestureDetector = new GestureDetector(context, mGestureListener);
+        mGestureDetector.setOnDoubleTapListener(mOnDoubleTapListener);
     }
 
     @Override
@@ -35,7 +35,7 @@ public class GestureController extends Controller {
     @Override
     protected void initController(@NonNull Context context) {
         super.initController(context);
-        mLayoutVolumeChange = findViewById(R.id.layout_volume_change);
+        mProgressVolume = findViewById(R.id.progress_volume);
     }
 
     @Override
@@ -47,43 +47,97 @@ public class GestureController extends Controller {
     public boolean onTouchEvent(MotionEvent event) {
         final int action = event.getAction();
         switch (action) {
-            case MotionEvent.ACTION_DOWN:
-                mLastMotionX = event.getX();
-                mLastMotionY = event.getY();
-                break;
-            case MotionEvent.ACTION_MOVE:
-                float x = event.getX();
-                float y = event.getY();
-
-                float dx = x - mLastMotionX;
-                float dy = y - mLastMotionY;
-
-                float xDiff = Math.abs(dx);
-                float yDiff = Math.abs(dy);
-
-                mLastMotionX = x;
-                mLastMotionY = y;
-
-                showVolumeChange();
-                break;
             case MotionEvent.ACTION_CANCEL:
             case MotionEvent.ACTION_UP:
-                hideVolumeChange();
+                mVolChanging = false;
+                mProgressVolume.setVisibility(GONE);
                 break;
         }
-        return true;
+        return mGestureDetector.onTouchEvent(event);
     }
 
-    private void showVolumeChange() {
-        KLog.d(TAG, "");
-        mLayoutVolumeChange.setVisibility(VISIBLE);
 
-        ProgressBar progress_volume = mLayoutVolumeChange.findViewById(R.id.progress_volume);
-        TextView tv_volume = mLayoutVolumeChange.findViewById(R.id.tv_volume);
-    }
+    private GestureDetector.OnGestureListener mGestureListener = new GestureDetector.OnGestureListener() {
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
 
-    private void hideVolumeChange() {
-        KLog.d(TAG, "");
-        mLayoutVolumeChange.setVisibility(GONE);
-    }
+        @Override
+        public void onShowPress(MotionEvent e) {
+            KLog.d(TAG, "e = " + e);
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            KLog.d(TAG, "e = " + e);
+            return true;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent downEvent, MotionEvent event, float distanceX, float distanceY) {
+            float moveX = event.getX() - downEvent.getX();
+            float moveY = event.getY() - downEvent.getY();
+
+            float xDiff = Math.abs(moveX);
+            float yDiff = Math.abs(moveY);
+
+            if (!mVolChanging) {
+                if (yDiff > xDiff) {
+                    mVolChanging = true;
+                    mProgressVolume.setVisibility(VISIBLE);
+                    AudioManager am = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
+                    mVolume = am.getStreamVolume(AudioManager.STREAM_MUSIC);
+                }
+            } else {
+                int dVol = (int) (-moveY * 0.02);
+
+                int streamType = AudioManager.STREAM_MUSIC;
+                AudioManager am = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
+                am.setStreamVolume(streamType, mVolume + dVol, 0);
+
+                int volume = am.getStreamVolume(streamType);
+                int maxVolume = am.getStreamMaxVolume(streamType);
+
+                mProgressVolume.setProgress((int) (volume * 1f / maxVolume * mProgressVolume.getMax()));
+            }
+            return false;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            KLog.d(TAG, "e = " + e);
+        }
+
+        @Override
+        public boolean onFling(MotionEvent downEvent, MotionEvent event, float velocityX, float velocityY) {
+            KLog.d(TAG, "velocityX = " + velocityX + ", velocityY = " + velocityY);
+            return true;
+        }
+    };
+
+    private GestureDetector.OnDoubleTapListener mOnDoubleTapListener = new GestureDetector.OnDoubleTapListener() {
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            KLog.i(TAG, "e = " + e);
+            if (isShowing()) {
+                hide();
+            } else {
+                show();
+            }
+            return true;
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            KLog.i(TAG, "e = " + e);
+            return true;
+        }
+
+        @Override
+        public boolean onDoubleTapEvent(MotionEvent e) {
+            KLog.i(TAG, "e = " + e);
+            return true;
+        }
+    };
 }
