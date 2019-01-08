e2bc27d8d0172a7d3abe4cca5dad6ea6850060eb
LuWei <luwei@ykecf.cn>
2019-01-08 17:43:17 +0800

    调节声量和亮度

--- app/src/main/java/com/loosu/soplayer/widget/videoview/controller/GestureController.java
+++ app/src/main/java/com/loosu/soplayer/widget/videoview/controller/GestureController.java
@@ -1,11 +1,13 @@
 package com.loosu.soplayer.widget.videoview.controller;
 
+import android.app.Activity;
 import android.content.Context;
 import android.media.AudioManager;
 import android.support.annotation.NonNull;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
-import android.view.ViewConfiguration;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.loosu.soplayer.R;
 import com.loosu.soplayer.utils.KLog;
@@ -15,9 +17,13 @@ public class GestureController extends Controller {
     private static final String TAG = "GestureController";
 
     private boolean mVolChanging = false;
+    private boolean mScreenBrightChanging = false;
+
     private int mVolume;
+    private float mScreenBrightness;
 
     private SoProgressBar mProgressVolume;
+    private SoProgressBar mProgressScreenBright;
 
     private final GestureDetector mGestureDetector;
 
@@ -36,6 +42,7 @@ public class GestureController extends Controller {
     protected void initController(@NonNull Context context) {
         super.initController(context);
         mProgressVolume = findViewById(R.id.progress_volume);
+        mProgressScreenBright = findViewById(R.id.progress_screen_brightness);
     }
 
     @Override
@@ -50,7 +57,9 @@ public class GestureController extends Controller {
             case MotionEvent.ACTION_CANCEL:
             case MotionEvent.ACTION_UP:
                 mVolChanging = false;
+                mScreenBrightChanging = false;
                 mProgressVolume.setVisibility(GONE);
+                mProgressScreenBright.setVisibility(GONE);
                 break;
         }
         return mGestureDetector.onTouchEvent(event);
@@ -76,20 +85,32 @@ public class GestureController extends Controller {
 
         @Override
         public boolean onScroll(MotionEvent downEvent, MotionEvent event, float distanceX, float distanceY) {
+            float downX = downEvent.getX();
+            float downY = downEvent.getY();
+
             float moveX = event.getX() - downEvent.getX();
             float moveY = event.getY() - downEvent.getY();
 
             float xDiff = Math.abs(moveX);
             float yDiff = Math.abs(moveY);
 
-            if (!mVolChanging) {
+            if (!mVolChanging && !mScreenBrightChanging) {
                 if (yDiff > xDiff) {
-                    mVolChanging = true;
-                    mProgressVolume.setVisibility(VISIBLE);
-                    AudioManager am = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
-                    mVolume = am.getStreamVolume(AudioManager.STREAM_MUSIC);
+                    if (downX > getWidth() / 2) {
+                        mVolChanging = true;
+                        mProgressVolume.setVisibility(VISIBLE);
+                        AudioManager am = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
+                        mVolume = am.getStreamVolume(AudioManager.STREAM_MUSIC);
+                    } else {
+                        mScreenBrightChanging = true;
+                        mProgressScreenBright.setVisibility(VISIBLE);
+                        if (getContext() instanceof Activity) {
+                            WindowManager.LayoutParams windowParams = ((Activity) getContext()).getWindow().getAttributes();
+                            mScreenBrightness = windowParams.screenBrightness;
+                        }
+                    }
                 }
-            } else {
+            } else if (mVolChanging) {
                 int dVol = (int) (-moveY * 0.02);
 
                 int streamType = AudioManager.STREAM_MUSIC;
@@ -100,7 +121,27 @@ public class GestureController extends Controller {
                 int maxVolume = am.getStreamMaxVolume(streamType);
 
                 mProgressVolume.setProgress((int) (volume * 1f / maxVolume * mProgressVolume.getMax()));
+
+            } else if (mScreenBrightChanging) {
+                if (getContext() instanceof Activity) {
+
+                    float changeBright = (float) (-moveY * 0.002);
+
+                    Window window = ((Activity) getContext()).getWindow();
+                    WindowManager.LayoutParams params = window.getAttributes();
+
+                    params.screenBrightness = mScreenBrightness + changeBright;
+                    if (params.screenBrightness > 1) {
+                        params.screenBrightness = 1;
+                    } else if (params.screenBrightness < 0) {
+                        params.screenBrightness = 0;
+                    }
+                    window.setAttributes(params);
+
+                    mProgressScreenBright.setText(String.format("%.1f",params.screenBrightness));
+                }
             }
+
             return false;
         }
 
